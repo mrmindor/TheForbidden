a90f62d6d5012e641618af515b1bbd2c70533306
mrmindor <mr.mindor@gmail.com>
2015-02-23 19:58:20 -0600

    fixed missing SqlClient reference

--- src/Model.Test/CardDesignTest.cs
+++ src/Model.Test/CardDesignTest.cs
@@ -1,5 +1,7 @@
 ﻿using Model;
 using NUnit.Framework;
+using System.Data;
+using System.Linq;
 
 namespace Model.Test
 {
@@ -9,9 +11,10 @@ namespace Model.Test
         [Test]
         public void ShouldLoadCards()
         {
+            //System.Data.SqlClient
             var dao = new CardDesignEntities();
-            var x = dao.AlchemyCards;
-            var y = dao.AlchemyElements;
+            var x = dao.AlchemyCards.ToList();
+            var y = dao.AlchemyElements.ToList();
             Assert.That(y,Is.Not.Empty);
         }
     }
